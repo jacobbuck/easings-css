110f694a296e7cfd34b34227f9413d858336ac04
Jacob Buck <buck.jacob@gmail.com>
2020-09-13 05:53:45 +0000

    Add templates.js

---
+++ build/templates.js
@@ -0,0 +1,49 @@
+// Converts a string from camelCase to kebab-case.
+const kebabCase = (string) =>
+  string.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`);
+
+const css = (easings) =>
+  ':root {' +
+  Object.entries(easings)
+    .flatMap(([name, value]) => [
+      `--${name}: ${value};`,
+      `--${kebabCase(name)}: ${value};`,
+    ])
+    .join('') +
+  '}';
+
+const js = (easings) =>
+  Object.entries(easings)
+    .map(([name, value]) => `const ${name} = '${value}';`)
+    .join('') +
+  'export {' +
+  Object.keys(easings).join(',') +
+  '}';
+
+const json = (easings) =>
+  '{' +
+  Object.entries(easings)
+    .flatMap(([name, value]) => [
+      `"${name}": "${value}"`,
+      `"${kebabCase(name)}": "${value}"`,
+    ])
+    .join(',') +
+  '}';
+
+const less = (easings) =>
+  Object.entries(easings)
+    .flatMap(([name, value]) => [
+      `@${name}: ${value};`,
+      `@${kebabCase(name)}: ${value};`,
+    ])
+    .join('');
+
+const scss = (easings) =>
+  Object.entries(easings)
+    .flatMap(([name, value]) => [
+      `$${name}: ${value};`,
+      `$${kebabCase(name)}: ${value};`,
+    ])
+    .join('');
+
+module.exports = { css, js, json, less, scss };
