30bfe3d1b8234aba321c2e43fd3e59840e1c9b2e
Jacob Buck <buck.jacob@gmail.com>
2020-09-17 09:59:33 +0000

    Refactor build script to use async functions and use prettier config

--- build/index.js
+++ build/index.js
@@ -1,19 +1,39 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const prettier = require('prettier');
 const easings = require('../index.json');
 const templates = require('./templates');
 
-const outputs = [
-  ['index.css', templates.css, 'css'],
-  ['index.js', templates.js, 'babel'],
-  ['index.less', templates.less, 'less'],
-  ['_index.scss', templates.scss, 'scss'],
-];
+(async function () {
+  const outputs = [
+    ['index.css', templates.css, 'css'],
+    ['index.js', templates.js, 'babel'],
+    ['index.less', templates.less, 'less'],
+    ['_index.scss', templates.scss, 'scss'],
+  ];
 
-for (const [file, template, type] of outputs) {
-  const data = prettier.format(template(easings), { parser: type });
-  fs.writeFile(file, data, (error) => {
-    if (error) throw error;
-    console.info(`📝 ${file} was written.`);
+  const options = await prettier.resolveConfig(__dirname);
+
+  const written = await Promise.all(
+    outputs.map(async ([file, template, type]) => {
+      try {
+        const data = prettier.format(template(easings), {
+          ...options,
+          parser: type,
+        });
+        await fs.writeFile(file, data);
+        return { file };
+      } catch (error) {
+        return { error, file };
+      }
+    })
+  );
+
+  written.forEach(({ error, file }) => {
+    if (error) {
+      console.info(`🛑 There was an issue building "${file}":`);
+      console.error(error);
+    } else {
+      console.info(`📝 "${file}" created sucessfully!`);
+    }
   });
-}
+})();
